8b292b1628a7238d766208bed5d2f5f098ea38a8
João Lourenção <jotap.lourencao@gmail.com>
2024-04-05 20:36:44 -0300

    fix: search field

--- curso_django/projeto1/recipes/views.py
+++ curso_django/projeto1/recipes/views.py
@@ -1,13 +1,14 @@
-from django.http import Http404
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
+
 from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().filter(
-        is_published=True
+    recipes = Recipe.objects.filter(
+        is_published=True,
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -17,7 +18,7 @@ def category(request, category_id):
     recipes = get_list_or_404(
         Recipe.objects.filter(
             category__id=category_id,
-            is_published=True
+            is_published=True,
         ).order_by('-id')
     )
 
@@ -28,9 +29,18 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
     })
+
+
+def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
+    return render(request, 'recipes/pages/search.html')
